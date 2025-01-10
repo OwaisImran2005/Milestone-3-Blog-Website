de9f6eb278c9c598c12d02271d304b35d00d1b36
OwaisImran2005 <owaisimran55555@gmail.com>
2025-01-11 01:33:38 +0500

    Update page.tsx

--- src/app/Blog2/page.tsx
+++ src/app/Blog2/page.tsx
@@ -1,16 +1,7 @@
-import { FC } from 'react';
+
 import CommentSection from '../components/commentSec/page';
 import Header from '../components/Header/page';
-import dodgec from "../../../images/dchlng.png"
-import Link from 'next/link';
 import bugh from "../../../images/bugatti-png.png"
-import mac from "../../../images/mclaren-png.png"
-import lam from "../../../images/lambo.jpg"
-import am from "../../../images/aston-martin-png.png"
-import cam from "../../../images/camaro.png"
-import ferr from "../../../images/ferrari.png"
-import gtr from "../../../images/gtr.png"
-import por from "../../../images/porsche.png"
 import Footer from '../components/Footer/page';
 import Blogcards from '../Blogcards/page';
 import Image from 'next/image';
@@ -33,7 +24,7 @@ export default function Blog2() {
                         same thing for topic Bugatti Chiron: A Masterpiece of Automotive Engineering,With an 8.0-liter <br /> quad-turbocharged W16 engine delivering 1,479 horsepower, the Chiron accelerates from 0 to 60 mph in just <br /> 2.4 seconds.,Crafted from the finest materials, its luxurious interior offers comfort and sophistication. Equipped <br /> with cutting-edge technology, the Chiron ensures
                         The Bugatti Chiron is truly a masterpiece of automotive <br /> engineering. With an 8.0-liter br quad-turbocharged W16 engine delivering an astonishing 1,479 horsepower <br />the Chiron rockets from 0 to 60 mph in just 2.4 seconds. This unparalleled performance places it at the pinnacle <br /> of high-performance sports cars.
 
-                        Crafted from the finest materials, the Chiron's luxurious interior exudes comfort <br /> and sophistication, offering an unparalleled driving experience. The attention to detail in its design ensures that <br /> every element of the cabin is both functional and aesthetically pleasing.
+                        Crafted from the finest materials, the Chiron`s luxurious interior exudes comfort <br /> and sophistication, offering an unparalleled driving experience. The attention to detail in its design ensures that <br /> every element of the cabin is both functional and aesthetically pleasing.
 
                         Equipped with cutting-edge technology, 
                         <br />
@@ -42,8 +33,7 @@ export default function Blog2() {
 
                         The legacy of the Bugatti Chiron extends beyond its impressive specifications. It stands as a testament to human ingenuity, pushing the boundaries of what is possible in automotive engineering. The Chiron inspires awe and admiration, securing its place in the annals of automotive history as one of the greatest cars ever made.
                         Bugatti Chiron will undoubtedly endure, inspiring future generations of automotive engineers and designers to reach for new heights.
-
-By pushing the boundaries of what's possible, the Bugatti Chiron remains a pinnacle of human achievement in the realm of automotive engineering, solidifying its place as a timeless masterpiece.
+By pushing the boundaries of what`s possible, the Bugatti Chiron remains a pinnacle of human achievement in the realm of automotive engineering, solidifying its place as a timeless masterpiece.
                     </p>
                 </div>
             </div>
