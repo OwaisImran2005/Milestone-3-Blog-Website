9a33614368bb2f21e411821eeae35d6f933be81d
OwaisImran2005 <owaisimran55555@gmail.com>
2025-01-11 01:37:45 +0500

    Update page.tsx

--- src/app/Blog4/page.tsx
+++ src/app/Blog4/page.tsx
@@ -1,16 +1,7 @@
-import { FC } from 'react';
+
 import CommentSection from '../components/commentSec/page';
 import Header from '../components/Header/page';
-import dodgec from "../../../images/dchlng.png"
-import Link from 'next/link';
-import bugh from "../../../images/bugatti-png.png"
-import mac from "../../../images/mclaren-png.png"
-import lam from "../../../images/lambo.jpg"
-import am from "../../../images/aston-martin-png.png"
-import cam from "../../../images/camaro.png"
 import ferr from "../../../images/ferrari.png"
-import gtr from "../../../images/gtr.png"
-import por from "../../../images/porsche.png"
 import Footer from '../components/Footer/page';
 import Blogcards from '../Blogcards/page';
 import Image from 'next/image';
@@ -35,7 +26,7 @@ export default function Blog4() {
 
 With a rich history rooted in motorsport, Ferrari has always been at the forefront of innovation and design. Their cars, such as the legendary F40, the elegant 458 Italia, and the groundbreaking LaFerrari, are celebrated for their breathtaking aesthetics and exhilarating performance. Every detail of a Ferrari is meticulously crafted to deliver an unforgettable driving experience.
 
-Ferrari's interiors are the epitome of luxury, featuring the finest materials and sophisticated design.
+Ferrari`s interiors are the epitome of luxury, featuring the finest materials and sophisticated design.
 <br />
 <br />
 
@@ -43,7 +34,7 @@ Ferrari's interiors are the epitome of luxury, featuring the finest materials an
 
 The performance of a Ferrari is nothing short of extraordinary. With powerful engines and advanced aerodynamics, Ferrari cars deliver blistering speed and precise handling. Whether on the racetrack or the open road, driving a Ferrari is an experience that engages all the senses.The legacy of Ferrari is built on a foundation of passion, innovation, and a relentless pursuit of perfection. This commitment to excellence has made Ferrari a symbol of status and achievement, revered by collectors and enthusiasts worldwide.
 
-Ferrari's influence extends far beyond the automotive industry. The brand's dedication to pushing the boundaries of performance and design has inspired countless innovations and set new standards for luxury and speed.
+Ferrari`s influence extends far beyond the automotive industry. The brand`s dedication to pushing the boundaries of performance and design has inspired countless innovations and set new standards for luxury and speed.
           </p>
 
 </div>
